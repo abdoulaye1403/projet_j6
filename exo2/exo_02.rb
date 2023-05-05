562a513915889a0d86412d7f94d0e01413761cc5
Abdoulaye Toure <abdelraul140385@gmail.com>
2023-05-05 23:14:55 +0000

    modifie

--- exo2/exo_02.rb
+++ exo2/exo_02.rb
@@ -3,3 +3,6 @@ puts "Et avec une voix sexy, ça donne: Bonjour, monde !"
 
 print "Bonjour, monde !"
 print "Et avec une voix sexy, ça donne: Bonjour, monde !"
+
+#puts fais un retour en ligne 
+#print ne fais pas de retour en ligne
